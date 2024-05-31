5b220458a6f042c7f6cca82697b9843d0e9fccd9
jszental <64125098+jszental@users.noreply.github.com>
2024-06-01 09:36:52 +1000

    roles, favicon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "751229652"
+    serviceWorkerVersion: "1715734106"
   }
 });
